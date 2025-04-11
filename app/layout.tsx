dec8a081407241f59ccb35cc3ed7ec681e13174b
omatty123 <omatty@gmail.com>
2025-04-11 07:06:48 -0500

    Add globals.css (resolved conflict)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
+import './globals.css'; // Make sure this file exists
+
 export const metadata = {
-  title: "Translation Website",
-  description: "Academic Korean to English Translation by Matty Wegehaupt",
+  title: 'Korean to English Academic Translation',
+  description: 'Academic translation by Matty',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
